8f4d6a3a83377278eb2c730510895d73b4f75b17
XrayAD <59537934+XrayAD@users.noreply.github.com>
2021-12-20 15:11:21 -0800

    Delete pascal.py

--- pascal.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        new = [0] * numRows
-        for i in range(numRows):
-            new[i] = [1] * (i+1)
-        for i in range(numRows):
-            for j in range(1, i):
-                new[i][j] = new[i-1][j-1] + new[i-1][j]
-        return new
