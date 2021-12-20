bf89907534ed6845a2c942f42bdf2b21faf134db
XrayAD <59537934+XrayAD@users.noreply.github.com>
2021-12-20 15:06:04 -0800

    Create reverse_string.py

---
+++ strings/reverse_string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        mid = int(len(s)/2)
+        for i in range(mid):
+            s[i],s[-i-1] = s[-i-1],s[i]
+        
